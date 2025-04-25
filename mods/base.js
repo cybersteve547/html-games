245570d6fa1bc0c79ed236724d7d4e608c9a6915
cybersteve547 <minecraftstevso@gmail.com>
2025-04-25 01:12:17 -0400

    Update base.js

--- mods/base.js
+++ mods/base.js
@@ -1,8 +1,4 @@
 levels.start = {
   text: 'You are going on a perilous journey, what will you bring with you?',
-  options: [
-    "food",
-    "phone",
-    "house"
-  ]
+  options: ["food", "phone", "house"]
 }
