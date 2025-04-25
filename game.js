e1cb6be71dc1cfde55b92f58c11488f025583b7b
cybersteve547 <minecraftstevso@gmail.com>
2025-04-25 01:38:59 -0400

    Update game.js

--- game.js
+++ game.js
@@ -7,8 +7,15 @@ const script = document.createElement("script");
 script.src = `mods/base.js`;
 document.body.appendChild(script);
 script.onload = () => {
-    document.getElementById("nameDisplay").innerText = `Name: ${name}`;
-    document.getElementById("textDisplay").innerText = levels[level].text || "Level not found.";
+    const leveldata = levels[level]
+    document.getElementById("nameDisplay").textContent = `Name: ${name}`;
+    document.getElementById("textDisplay").textContent = leveldata.text || "Level not found.";
+    let options = []
+    for (let i = 0; i < leveldata.options.length; i++) {
+        options.push(document.createElement("button"));
+        options[i].textContent = leveldata.options[i]
+        document.body.appendChild(options[i]);
+    };
 };
 script.onerror = () => {
     document.getElementById("textDisplay").innerText = "Failed to load mod.";
