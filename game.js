442c1c1579eacbf36dacfb9c857bb43103cc0b7b
cybersteve547 <minecraftstevso@gmail.com>
2025-04-25 00:06:24 -0400

    Update game.js

--- game.js
+++ game.js
@@ -3,8 +3,9 @@ const level = url.searchParams.get("level");
 const name = url.searchParams.get("name");
 const mod = url.searchParams.get("mod");
 let levels = []
+debug "nooo"
 const script = document.createElement("script");
 script.src = `mods/${mod}.js`;
 document.body.appendChild(script);
 document.getElementById("nameDisplay").innerText = `Name: ${name}`;
-document.getElementById("textDisplay").innerText = levels[level]?.text || "Level not found.";
+document.getElementById("textDisplay").innerText = levels[level]?.text || debug;
