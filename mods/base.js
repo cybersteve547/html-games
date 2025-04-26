86947b5355281af4d55e078d2b97d585b123341d
cybersteve547 <minecraftstevso@gmail.com>
2025-04-26 00:57:24 -0400

    Update base.js

--- mods/base.js
+++ mods/base.js
@@ -1,9 +1,9 @@
 levels.start = {
   text: "You are going on a perilous journey, what will you bring with you?",
-  options: ["food", "phone", "house"]
+  options: [{text: "food", level: "crossroads"}, {text: "phone", level: "crossroads"}, {text: "house", level: "crossroads"]
 };
 
 levels.backrooms = {
   text: "If you're not careful and you noclip out of reality in the wrong areas, you'll end up in the Backrooms, where it's nothing but the stink of moist carpet, the...",
-  options: ["RUN."]
+  options: [{text: "RUN", level: "backrooms"}]
 };
