de8034a14571eb1f5d5cc556292bac31de92fb88
cybersteve547 <minecraftstevso@gmail.com>
2025-04-28 19:34:00 -0400

    Update game.js

--- game.js
+++ game.js
@@ -3,11 +3,15 @@ const level = url.searchParams.get("level");
 const name = url.searchParams.get("name");
 const mod = url.searchParams.get("mod");
 let levels = {};
+levels.backrooms = {
+  text: "If you're not careful and you noclip out of reality in the wrong areas, you'll end up in the Backrooms, where it's nothing but the stink of moist carpet, the...",
+  options: [{text: "RUN", level: "backrooms"}]
+};
 const script = document.createElement("script");
 script.src = `mods/base.js`;
 document.body.appendChild(script);
 script.onload = () => {
-    const leveldata = levels[level] || levels.backrooms || {text: "barkoorms", options: [{text: "ruun", level: ""}]};
+    const leveldata = levels[level] || levels.backrooms
     document.getElementById("nameDisplay").textContent = `Name: ${name}`;
     document.getElementById("textDisplay").textContent = leveldata.text || "Level not found.";
     let options = [];
