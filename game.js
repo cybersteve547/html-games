6e5d35c63426bed71963aef4b03485762c2b0f2c
cybersteve547 <minecraftstevso@gmail.com>
2025-04-28 20:30:55 -0400

    Update game.js

--- game.js
+++ game.js
@@ -2,6 +2,7 @@ const url = new URL(window.location.href);
 const level = url.searchParams.get("level");
 const name = url.searchParams.get("name");
 const mod = url.searchParams.get("mod");
+let inventory = [];
 let levels = {};
 levels.backrooms = {
   text: "If you're not careful and you noclip out of reality in the wrong areas, you'll end up in the Backrooms, where it's nothing but the stink of moist carpet, the...",
